5e2b33a1ffd2688077b294c3abd451458436f72a
Abhay Kumar Agrawal <101392842+AbhayAg10@users.noreply.github.com>
2023-09-08 19:40:12 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,32 @@
+//while loop
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index++;
+// }
+
+// let arr1 = ['flash', 'superman', 'batman'];
+// let val = 0;
+
+// while (val < arr1.length) {
+//     console.log(`Value of arr1 is ${arr1[val]}`);
+//     val++;
+// }
+
+
+//dowhile loop
+
+// let score = 1;
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+// let score = 11;
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
